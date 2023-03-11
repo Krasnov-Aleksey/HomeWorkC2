c4e475b7a53b8f89de93075853ab7cb7f84aa5c9
aleksey.krasnov <krasnov.80@mail.ru>
2023-03-11 08:41:34 +0300

    Задание 15

---
+++ Task15/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.Write("Введите день недели числом ");
+int dayWeek=Convert.ToInt32(Console.ReadLine());
+while (dayWeek<1 || dayWeek>7)
+{
+    Console.WriteLine("Вы ввели неверное число ");
+    Console.Write("Введите день недели числом ");
+    dayWeek=Convert.ToInt32(Console.ReadLine());
+}
+if (dayWeek==6 || dayWeek==7)
+{
+    Console.WriteLine("Выходной");
+}
+else
+{
+    Console.WriteLine("Рабочий");
+}
+
