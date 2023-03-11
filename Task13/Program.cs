7c506dec6cd22c911ce4874de3a7dab1576ac56f
aleksey.krasnov <krasnov.80@mail.ru>
2023-03-11 08:25:46 +0300

    Задание 13

---
+++ Task13/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.Write("Введите число ");
+int namber=Convert.ToInt32(Console.ReadLine());
+if (namber>999)
+{
+    while (namber>999)
+    {
+        namber=namber/10;
+    }
+    namber=namber%10;
+    Console.WriteLine(namber);
+}
+else
+{
+    Console.WriteLine("Третей цыфры нет");
+}
+
+
